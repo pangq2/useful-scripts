eaeb38c9b2943f19b13ce8c0d49e2a55761a7b7a
pangq2 <pangq@oregonstate.edu>
2018-05-18 19:09:21 -0700

    Add the charge.py

---
+++ useful-scripts/charge.py
@@ -0,0 +1,25 @@
+import os
+
+def read_ACF(filename,info)
+    """ Thie is used to read the ACF.dat and return the charge
+    parameters
+    -----------
+    ACF.dat
+    info 
+        information from POSCAR and POTCAR
+    return
+    -----------
+    return the charge of the selected atoms
+
+    """
+
+def read_files()
+    """ This is used to extract info from the POSCAR and POTCAR
+    parameters
+    ---------------
+    POSCAR, POTCAR
+
+    return
+    ------------
+    The information needed for the charge analysis
+    """
